5d625d13b965502aab7558ca9c34012c4f3c3774
Aleksandr Rudakov <alexandr.rudakov@modera.net>
2014-02-18 11:28:15 +0200

    Prallel tests

--- toxer/cli.py
+++ toxer/cli.py
@@ -1,7 +1,14 @@
 import argparse
+from subprocess import Popen, PIPE
+from time import sleep
+import sys
 import os
 from voluptuous import Schema
 import yaml
+
+
+def call(cmd):
+    return Popen(cmd, stdout=PIPE, shell=True).communicate()[0].strip()
 
 
 def build_images():
@@ -32,6 +39,7 @@ def main():
     parser.add_argument('--cov', action='store_true', default=False)
     parser.add_argument('--dist', default=False)
     parser.add_argument('--env', default=False)
+    parser.add_argument('-p', '--parallel', action='store_true', default=False)
     parser.add_argument('-m', '--marks', default=False)
     parser.add_argument('-s', action='store_true', default=False)
     parser.add_argument('--coverage-dir', default='coverage_html')
@@ -73,17 +81,22 @@ def main():
     else:
         all_distr = config['images'].keys()
 
+    processes_running = []
     for image, info in config['images'].items():
 
         if not image in all_distr:
             continue
 
-        print('*' * 60)
-        print(image)
-        print('*' * 60)
+        if args.parallel:
+            running_mode = '-d'
+        else:
+            running_mode = '-i -t'
 
-        cmd = 'docker run -w "/code" -e "TOX_DOCKER=1" -e "TOX_DISTRO=%s" -v %s:/code -i -t %s' % (
-            info['image'], os.getcwd(), info['image'])
+        cmd = 'docker run %(mode)s -w "/code" -e "TOX_DOCKER=1" -e "TOX_DISTRO=%(image)s" -v %(cwd)s:/code %(image)s' % {
+            'image': info['image'],
+            'cwd': os.getcwd(),
+            'mode': running_mode
+        }
 
         envs = info['envs']
 
@@ -96,28 +109,50 @@ def main():
             print('Skip %s as there is no env needed [%s]' % (image, ','.join(envs)))
             continue
 
-        cmd += ' tox -c tox.toxer.ini -e %s' % (','.join(env_to_run))
+        for env in env_to_run:
+            cmd_env = cmd + ' tox -c tox.toxer.ini -e %s' % env
 
-        cmd += ' --'
+            cmd_env += ' --'
 
-        if args.cov:
-            cmd += ' --cov-config .tox.coveragerc --cov %s' % config['packages']['code']
+            if args.cov:
+                cmd_env += ' --cov-config .tox.coveragerc --cov %s' % config['packages']['code']
 
-        if args.s:
-            cmd += ' -s'
+            if args.s:
+                cmd_env += ' -s'
 
-        os.system(cmd)
-        print cmd
+            if args.parallel:
+                process_id = call(cmd_env)
+                processes_running.append((process_id, image, env))
+            else:
+                os.system(cmd_env)
 
-    if args.cov:
+    if args.parallel:
 
-        print('collecting coverage.')
+        while processes_running:
+            sleep(0.5)
 
-        cmd = 'docker run -w "/code" -v %s:/code -i -t %s' % (
-            os.getcwd(), config['coverage']['image'])
+            sys.stdout.write('.')
 
-        os.system('%s coverage combine' % cmd)
-        os.system('%s coverage html --include="%s/*" -d %s' % (cmd, config['packages']['code'], args.coverage_dir))
+            for process_info in processes_running:
 
-        print('Coverage link: file://%s/%s/index.html' % (os.getcwd(), args.coverage_dir))
+                process, image, env = process_info
+
+                is_running = call('docker inspect --format=\'{{.State.Running}}\' %s' % process) == "true"
+
+                if not is_running:
+                    processes_running.remove(process_info)
+
+                    ret = int(call('docker inspect --format=\'{{.State.ExitCode}}\' %s' % process))
+
+                    print('%s:%s ... done' % (image, env))
+                    print('ret code: %s' % ret)
+                    if ret != 0:
+                        print('=' * 40)
+                        print('=' * 40)
+                        os.system('docker logs %s' % process)
+                        print('=' * 40)
+                        print('=' * 40)
+                    else:
+                        print('ok')
+
 
